b77efdbd74406b8fd2d85ba643b37d72b4f730fd
nikhil.s.richie@gmail.com <nikhil.s.richie@gmail.com>
2025-07-28 12:50:06 +0530

    made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
-import "./App.css";
 
 function App() {
   return (
     <>
-      <div className="text-5xl bg-gray-200 font-bold h-screen flex items-center justify-center">
-        Hello Welcome To Excel Analysis Project
+      <div className='text-5xl bg-gray-200 font-bold h-screen flex items-center justify-center text-red-600'>
+       Hello Welcome To Excel Analysis Project
       </div>
     </>
   );
