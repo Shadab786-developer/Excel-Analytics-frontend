072196fe3f50bc8aa5a07a0dfda7a115b83c8411
Shadab Ahmed Qureshi <sq1597900@gmail.com>
2025-07-28 10:14:01 +0530

    Project setup done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-5xl bg-gray-200 font-bold h-screen flex items-center justify-center">
+        Hello Welcome To Excel Analysis Project
+      </div>
+    </>
+  );
+}
+
+export default App;
